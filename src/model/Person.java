c20670c9bb3f593ad44b795866b30d04b6133b9a
Asus <vaxa2320@gmail.com>
2024-01-08 17:17:59 +0300

    Initial commit

---
+++ src/model/Person.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.util.StringJoiner;
+
+public class Person {
+    private String name;
+    private int age;
+    private double salary;
+
+    public Person() {
+
+    }
+
+    public Person(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (Double.compare(person.salary, salary) != 0) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                " name='" + name + '\'' +
+                ", age=" + age +
+                ",salary=" + salary +
+                '}';
+    }
+}
+
+
+
